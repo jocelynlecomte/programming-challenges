e883e865147b3abc272d29ff3885309b65234df4
jocelyn <jocelyn.lecomte@gmail.com>
2020-06-01 08:20:36 +0200

    added competitive - pac (bronze league) WIP

--- src/main/java/org/lecomte/codingame/competitive/pac/Pac.kt
+++ src/main/java/org/lecomte/codingame/competitive/pac/Pac.kt
@@ -1,19 +1,24 @@
 package org.lecomte.codingame.competitive.pac
 
+import java.time.Instant
 import java.util.*
 
 const val WALL_CHAR = '#'
 const val EMPTY_CHAR = ' '
 const val SIMPLE_PELLET_VALUE = 1
 const val SUPER_PELLET_VALUE = 10
-const val SQUARED_SAFETY_DISTANCE = 4
+const val SAFETY_DISTANCE = 2
 
-fun log(message: String?) {
-    System.err.print(message)
-}
+fun log(message: Any?) = System.err.print(message)
 
-fun logln(message: String?) {
-    System.err.println(message)
+fun logln(message: Any?) = System.err.println(message)
+
+fun <T, U, V> cartesianProduct(c1: Collection<T>, c2: Collection<U>, combiner: (T, U) -> V): Set<V> {
+    return c1.flatMap { x ->
+        c2.map { y ->
+            combiner(x, y)
+        }
+    }.toSet()
 }
 
 enum class CellContent {
@@ -55,22 +60,20 @@ enum class PacType {
     abstract fun loseTo(): PacType
 }
 
-data class Point(val x: Int, val y: Int) {
-    fun squaredDistance(other: Point) = (x - other.x) * (x - other.x) + (y - other.y) * (y - other.y)
-
-    fun move(direction: Direction): Point {
+data class Pos(val x: Int, val y: Int) {
+    fun move(direction: Direction): Pos {
         return when (direction) {
-            Direction.NORTH -> Point(x, y - 1)
-            Direction.SOUTH -> Point(x, y + 1)
-            Direction.EAST -> Point(x + 1, y)
-            Direction.WEST -> Point(x - 1, y)
+            Direction.NORTH -> Pos(x, y - 1)
+            Direction.SOUTH -> Pos(x, y + 1)
+            Direction.EAST -> Pos(x + 1, y)
+            Direction.WEST -> Pos(x - 1, y)
         }
     }
 
     /**
      * computes the direction this point should go to move to the other point
      */
-    fun directionToMeet(other: Point): Direction = when {
+    fun directionToMeet(other: Pos): Direction = when {
         x > other.x -> Direction.WEST
         x < other.x -> Direction.EAST
         y > other.y -> Direction.NORTH
@@ -78,15 +81,15 @@ data class Point(val x: Int, val y: Int) {
     }
 }
 
-data class Pellet(val pos: Point, val value: Int) {
+data class Pellet(val pos: Pos, val value: Int) {
     val isSuper = value > 1
 }
 
-data class Pac(val id: Int, val mine: Boolean, val pos: Point, val previousPos: Point, val type: PacType,
-               val abilityCooldown: Int, val previousTarget: Point) {
+data class Pac(val id: Int, val mine: Boolean, val pos: Pos, val previousPos: Pos, val type: PacType,
+               val abilityCooldown: Int, val previousTarget: Pos) {
 
-    var target: Point = Point(0, 0)
-    var linesOfSight = mapOf<Direction, List<Point>>()
+    var target: Pos = Pos(0, 0)
+    var linesOfSight = mapOf<Direction, List<Pos>>()
 
     fun canUseAbility(): Boolean = abilityCooldown <= 0
 
@@ -120,14 +123,65 @@ data class Pac(val id: Int, val mine: Boolean, val pos: Point, val previousPos: 
 }
 
 class Game(private val width: Int, private val height: Int, private val grid: Array<String>) {
-    private val possiblePelletPositions = mutableSetOf<Point>()
+    private val middleRow = width / 2
+    private val walkablePositions = grid
+            .mapIndexed { rowIndex, row ->
+                row
+                        .mapIndexed { colIndex, char ->
+                            if (char == EMPTY_CHAR) Pos(colIndex, rowIndex) else null
+                        }
+                        .filterNotNull()
+            }
+            .flatten().toSet()
+
+    private val possiblePelletPositions = walkablePositions.toMutableSet()
+
+    private val shortestPaths = mutableMapOf<Pair<Pos, Pos>, List<Pos>>()
+
+    private val distance: (Pos, Pos) -> Int = { start, end -> computeShortestPath(start, end).size }
+    private val isForbidden: (Pos) -> Boolean = { pos -> pos.x < 0 || pos.x > width - 1 || pos.y < 0 || pos.y > height - 1 || WALL_CHAR == grid[pos.y][pos.x] }
+    private val isEnemy: (Pos) -> Boolean = { pos -> cellType(pos) == CellContent.ENEMY_PAC }
+    private val symetric: (Pos) -> Pos = { pos -> Pos(2 * middleRow - pos.x, pos.y) }
 
     init {
-        grid.forEachIndexed { rowIndex, row ->
-            row.forEachIndexed { colIndex, char ->
-                if (char == EMPTY_CHAR) possiblePelletPositions.add(Point(colIndex, rowIndex))
+        val walkableInLeftHalf = walkablePositions.filter { (x, _) -> x <= middleRow }
+        val walkableInRightHalf = walkablePositions.filter { (x, _) -> x >= middleRow }
+        val walkablePairsInLeftHalf = cartesianProduct(walkableInLeftHalf, walkableInLeftHalf) { pos1, pos2 ->
+            Pair(pos1, pos2)
+        }
+                .filterNot { (pos1, pos2) -> pos1 == pos2 }
+
+        val walkablePairsInEachHalf = cartesianProduct(walkableInLeftHalf, walkableInRightHalf) { pos1, pos2 ->
+            Pair(pos1, pos2)
+        }
+
+        logln("init walkable pairs ${walkablePairsInLeftHalf.size}")
+        val start = Instant.now()
+
+        walkablePairsInLeftHalf.forEach { (start, end) ->
+            val normalSearch = Pair(start, end)
+            val reversedsearch = Pair(end, start)
+            if (!shortestPaths.contains(normalSearch) && !shortestPaths.contains(reversedsearch)) {
+                val symetricSearch = Pair(symetric(start), symetric(end))
+                val path = computeShortestPath(start, end)
+                val symetricPath = path.map { symetric(it) }
+                shortestPaths[normalSearch] = path
+                shortestPaths[symetricSearch] = symetricPath
             }
         }
+
+        /*walkablePairsInEachHalf.forEach { (start, end) ->
+            val normalSearch = Pair(start, end)
+            val reversedsearch = Pair(end, start)
+            if (!shortestPaths.contains(normalSearch) && !shortestPaths.contains(reversedsearch)) {
+                val path = computeShortestPath(start, end)
+                shortestPaths[normalSearch] = path
+            }
+        }*/
+
+        val end = Instant.now()
+        logln("init, it took ${(end.toEpochMilli() - start.toEpochMilli())}")
+        logln("init, shortest paths ${shortestPaths.size}")
     }
 
     var score = 0
@@ -136,41 +190,73 @@ class Game(private val width: Int, private val height: Int, private val grid: Ar
     var pacs = setOf<Pac>()
     var visiblePellets = setOf<Pellet>()
 
-    private val isForbidden: (Point) -> Boolean = { pos -> pos.x < 0 || pos.x > width - 1 || pos.y < 0 || pos.y > height - 1 || cellType(pos) == CellContent.WALL }
-    private val isEnemy: (Point) -> Boolean = { pos -> cellType(pos) == CellContent.ENEMY_PAC }
+
+    fun computeShortestPaths(): Map<Pair<Pos, Pos>, List<Pos>> {
+        return mapOf()
+    }
+
+    internal fun computeShortestPath(start: Pos, end: Pos): List<Pos> {
+        if (isForbidden(start) || isForbidden(end)) {
+            return listOf()
+        }
+
+        val visited = mutableSetOf<Pos>()
+        val toVisit: Queue<Pair<Pos, List<Pos>>> = LinkedList()
+
+        var current = Pair(start, listOf<Pos>())
+        while (current.first != end) {
+            visited.add(current.first)
+            val unvisitedNeighbours = accessibleNeighbours(current.first).filterNot { visited.contains(it) }
+            toVisit.addAll(unvisitedNeighbours.map { Pair(it, current.second + it) })
+            current = toVisit.poll()
+        }
+        return current.second.dropLast(1)
+    }
+
+    internal fun accessibleNeighbours(pos: Pos): List<Pos> {
+        return Direction.values().mapNotNull { direction -> move(pos, direction) }
+    }
+
+    private fun move(pos: Pos, direction: Direction): Pos? {
+        val naiveNextPos = pos.move(direction)
+        val x = if (naiveNextPos.x < 0) width - 1 else if (naiveNextPos.x > width - 1) 0 else naiveNextPos.x
+        val result = Pos(x, naiveNextPos.y)
+
+        return if (isForbidden(result)) null else result
+    }
 
     fun updatePossiblePelletPositions() {
         val positionsWithoutPellet = pacs
                 .map { pac: Pac ->
                     val emptyPos = pac.linesOfSight.values
                             .flatten()
-                            .filter { pos: Point -> cellType(pos) == CellContent.EMPTY }
+                            .filter { pos: Pos -> cellType(pos) == CellContent.EMPTY }
                     emptyPos + pac.pos
                 }
                 .flatten()
         possiblePelletPositions.removeAll(positionsWithoutPellet)
-        logln("possible pellet positions ${possiblePelletPositions.size}")
     }
 
-    private fun randomPossiblePelletPosition(): Point {
+    private fun randomPossiblePelletPosition(): Pos {
         return possiblePelletPositions.random()
     }
 
-    private fun closestPossiblePelletPosition(pos: Point): Point {
-        return possiblePelletPositions.minBy(pos::squaredDistance) ?: Point(0, 0)
+    private fun closestPossiblePelletPosition(pos: Pos): Pos {
+        logln("in closest possible pellet pos for $pos, ${possiblePelletPositions.size}")
+        return possiblePelletPositions.minBy { distance(pos, it) } ?: Pos(0, 0)
     }
 
     fun computeLineOfSights() {
         friendlyPacs().forEach { pac -> pac.linesOfSight = lineOfSights(pac.pos) }
     }
 
-    private fun lineOfSights(from: Point): Map<Direction, List<Point>> {
+    private fun lineOfSights(from: Pos): Map<Direction, List<Pos>> {
         return Direction.values()
                 .map { direction -> direction to moveUntil(from.move(direction), direction, isForbidden) }
                 .associateBy({ it.first }, { it.second })
     }
 
-    private fun moveUntil(pos: Point, direction: Direction, predicate: (Point) -> Boolean, acc: List<Point> = listOf()): List<Point> {
+    private fun moveUntil(pos: Pos, direction: Direction, predicate: (Pos) -> Boolean, acc: List<Pos> = listOf()): List<Pos> {
         return if (predicate(pos)) {
             acc
         } else {
@@ -178,23 +264,23 @@ class Game(private val width: Int, private val height: Int, private val grid: Ar
         }
     }
 
-    private fun score(lineOfSight: List<Point>): Int {
+    private fun score(lineOfSight: List<Pos>): Int {
         return lineOfSight.map { pos -> cellScore(pos) }.sum()
     }
 
-    private fun cellScore(point: Point): Int {
-        return when (cellType(point)) {
+    private fun cellScore(pos: Pos): Int {
+        return when (cellType(pos)) {
             CellContent.SUPER_PELLET -> SUPER_PELLET_VALUE
             CellContent.PELLET -> SIMPLE_PELLET_VALUE
             else -> 0
         }
     }
 
-    private fun cellType(point: Point): CellContent {
-        val pac = pacs.find { pac -> pac.pos == point }
-        val pellet = visiblePellets.find { pellet -> pellet.pos == point }
+    private fun cellType(pos: Pos): CellContent {
+        val pac = pacs.find { pac -> pac.pos == pos }
+        val pellet = visiblePellets.find { pellet -> pellet.pos == pos }
         return when {
-            WALL_CHAR == grid[point.y][point.x] -> return CellContent.WALL
+            WALL_CHAR == grid[pos.y][pos.x] -> return CellContent.WALL
             pac != null -> return if (pac.mine) CellContent.FRIENDLY_PAC else CellContent.ENEMY_PAC
             pellet != null -> return if (pellet.isSuper) CellContent.SUPER_PELLET else CellContent.PELLET
             else -> CellContent.EMPTY
@@ -205,7 +291,7 @@ class Game(private val width: Int, private val height: Int, private val grid: Ar
 
     private fun visibleEnemyPacs() = pacs.filterNot { it.mine }
 
-    private fun findMostRewardingPelletPosition(pac: Pac): Point {
+    private fun findMostRewardingPelletPosition(pac: Pac): Pos {
         val superPelletPositions = visiblePellets.filter { it.isSuper }.map { it.pos }
         val visibleSimplePelletPositions = visiblePellets.filterNot { it.isSuper }.map { it.pos }
 
@@ -220,7 +306,7 @@ class Game(private val width: Int, private val height: Int, private val grid: Ar
 
         val closestSuperPelletPos = superPelletPositions
                 .filterNot { allTargets.contains(it) }
-                .minBy(pac.pos::squaredDistance)
+                .minBy { distance(pac.pos, it) }
 
         return if (closestSuperPelletPos != null) {
             closestSuperPelletPos
@@ -231,13 +317,13 @@ class Game(private val width: Int, private val height: Int, private val grid: Ar
             } else {
                 val closestVisiblePelletPos = visibleSimplePelletPositions
                         .filterNot { allTargets.contains(it) }
-                        .minBy(pac.pos::squaredDistance)
+                        .minBy { distance(pac.pos, it) }
                 closestVisiblePelletPos ?: closestPossiblePelletPosition(pac.pos)
             }
         }
     }
 
-    private fun findRunawayPosition(friendlyPac: Pac): Point {
+    private fun findRunawayPosition(friendlyPac: Pac): Pos {
         val availableLinesOfSight = friendlyPac.linesOfSight
                 .filterKeys { it != friendlyPac.direction() }
 
@@ -245,7 +331,7 @@ class Game(private val width: Int, private val height: Int, private val grid: Ar
         return findFarthestValuablePos ?: randomPossiblePelletPosition()
     }
 
-    private fun findEscapeCollisionTarget(friendlyPac: Pac): Point {
+    private fun findEscapeCollisionTarget(friendlyPac: Pac): Pos {
         val availableLinesOfSight = friendlyPac.linesOfSight
                 .filterValues { it.none { pos -> cellType(pos) == CellContent.FRIENDLY_PAC } }
 
@@ -254,19 +340,20 @@ class Game(private val width: Int, private val height: Int, private val grid: Ar
         return findFarthestValuablePos ?: randomPossiblePelletPosition()
     }
 
-    private fun findFarthestValuablePos(pos: Point, linesOfSight: Map<Direction, List<Point>>): Point? {
+    private fun findFarthestValuablePos(pos: Pos, linesOfSight: Map<Direction, List<Pos>>): Pos? {
         val bestDirection = linesOfSight
                 .map { entry -> Pair(entry.key, score(entry.value)) }
                 .maxBy { pair -> pair.second }!!.first
 
         val bestLineOfSight = linesOfSight.getValue(bestDirection)
-        return bestLineOfSight.dropLastWhile { cellScore(it) == 0 }.maxBy(pos::squaredDistance)
+        logln("best line of sight: $bestDirection -> $bestLineOfSight")
+        return bestLineOfSight.dropLastWhile { cellScore(it) == 0 }.maxBy { distance(pos, it) }
     }
 
     fun computeCommand(friendlyPac: Pac): String {
-        val enemyInSightPos = friendlyPac.linesOfSight.values.flatten().filter(isEnemy).minBy(friendlyPac.pos::squaredDistance)
+        val enemyInSightPos = friendlyPac.linesOfSight.values.flatten().filter(isEnemy).minBy { distance(friendlyPac.pos, it) }
         val enemyInSight = visibleEnemyPacs().find { pac -> pac.pos == enemyInSightPos }
-        val enemyWithinSafeDistance = visibleEnemyPacs().find { pac -> friendlyPac.pos.squaredDistance(pac.pos) < SQUARED_SAFETY_DISTANCE }
+        val enemyWithinSafeDistance = visibleEnemyPacs().find { pac -> distance(friendlyPac.pos, pac.pos) < SAFETY_DISTANCE }
         val enemy = enemyInSight ?: enemyWithinSafeDistance
 
         val action = if (enemy != null) {
@@ -299,12 +386,16 @@ class Game(private val width: Int, private val height: Int, private val grid: Ar
 
         val prefix = "${action.command} ${friendlyPac.id}"
 
+        logln("compute command for pac ${friendlyPac.id}: $action")
+
         friendlyPac.target = when (action) {
             Strategy.MOVE, Strategy.SPEED -> findMostRewardingPelletPosition(friendlyPac)
             Strategy.RUNAWAY -> findRunawayPosition(friendlyPac)
             Strategy.SWITCH -> friendlyPac.previousTarget
             Strategy.COLLISION -> findEscapeCollisionTarget(friendlyPac)
         }
+
+        logln("computed target for pac ${friendlyPac.id}: ${friendlyPac.target}")
 
         val command = when (action) {
             Strategy.SWITCH -> {
@@ -317,13 +408,16 @@ class Game(private val width: Int, private val height: Int, private val grid: Ar
                 "$prefix ${friendlyPac.target.x} ${friendlyPac.target.y}"
             }
         }
+
+        logln("computed command for pac ${friendlyPac.id}: $command")
+
         return "$command $action"
     }
 
     fun display() {
         grid.forEachIndexed { rowIndex, row ->
             row.forEachIndexed { colIndex, _ ->
-                val c = when (cellType(Point(colIndex, rowIndex))) {
+                val c = when (cellType(Pos(colIndex, rowIndex))) {
                     CellContent.WALL -> '#'
                     CellContent.FRIENDLY_PAC -> 'M'
                     CellContent.ENEMY_PAC -> 'X'
@@ -375,15 +469,15 @@ fun main(args: Array<String>) {
         for (i in 0 until visiblePacCount) {
             val pacId = input.nextInt() // pac number (unique within a team)
             val mine = input.nextInt() != 0 // true if this pac is yours
-            val newPos = Point(input.nextInt(), input.nextInt())
+            val newPos = Pos(input.nextInt(), input.nextInt())
             val typeId = input.next() // unused in wood leagues
             val speedTurnsLeft = input.nextInt() // unused in wood leagues
             val abilityCooldown = input.nextInt() // unused in wood leagues
 
             val type = PacType.valueOf(typeId)
             val previousPac = game.pacs.find { it.id == pacId }
-            val previousPos = previousPac?.pos ?: Point(0, 0)
-            val previousTarget = previousPac?.target ?: Point(0, 0)
+            val previousPos = previousPac?.pos ?: Pos(0, 0)
+            val previousTarget = previousPac?.target ?: Pos(0, 0)
             pacs.add(Pac(pacId, mine, newPos, previousPos, type, abilityCooldown, previousTarget))
         }
         game.pacs = pacs
@@ -394,7 +488,7 @@ fun main(args: Array<String>) {
             val x = input.nextInt()
             val y = input.nextInt()
             val value = input.nextInt() // amount of points this pellet is worth
-            pellets.add(Pellet(Point(x, y), value))
+            pellets.add(Pellet(Pos(x, y), value))
         }
         game.visiblePellets = pellets
 
--- src/test/java/org/lecomte/codingame/competitive/pac/GameTest.kt
+++ src/test/java/org/lecomte/codingame/competitive/pac/GameTest.kt
@@ -11,32 +11,84 @@ internal class GameTest {
     @BeforeEach
     fun setup() {
         val gridAsText = """
-            ###############################
-            #       #   #     #   #       #
-            # ### # # # # ### # # # # ### #
-            # ### #   #         #   # ### #
-            # ### ### ### ### ### ### ### #
-            #     #   #         #   #     #
-            ##### # # # # ### # # # # #####
-                    #   #     #   #        
-            ### # # ### ####### ### # # ###
-            #   # #                 # #   #
-            # ### ### ##### ##### ### ### #
-            #         #         #         #
-            ##### ### # # # # # # ### #####
-            #     #       # #       #     #
-            # # # # # # ####### # # # # # #
-            ###############################
+            0123456789012345678901234567890
+            ###############################0
+            #       #   #     #   #       #1
+            # ### # # # # ### # # # # ### #2
+            # ### #   #         #   # ### #3
+            # ### ### ### ### ### ### ### #4
+            #     #   #         #   #     #5
+            ##### # # # # ### # # # # #####6
+                    #   #     #   #        7
+            ### # # ### ####### ### # # ###8
+            #   # #                 # #   #9
+            # ### ### ##### ##### ### ### #0
+            #         #         #         #1
+            ##### ### # # # # # # ### #####2
+            #     #       # #       #     #3
+            # # # # # # ####### # # # # # #4
+            ###############################5
         """.trimIndent()
-        val grid = gridAsText.split(delimiters = *arrayOf("\n")).toTypedArray()
+        val grid = gridAsText.split(delimiters = *arrayOf("\n")).map { it.dropLast(1) }.drop(1).toTypedArray()
         game = Game(31, 16, grid)
+    }
+
+    @Nested
+    inner class TestNeighbours {
+        @Test
+        fun `should be OK for simple neighbours`() {
+            assertThat(game.accessibleNeighbours(Pos(1, 14))).contains(Pos(1, 13))
+            assertThat(game.accessibleNeighbours(Pos(1, 1))).contains(Pos(2, 1), Pos(1, 2))
+            assertThat(game.accessibleNeighbours(Pos(5, 7))).contains(Pos(5, 6), Pos(6, 7), Pos(5, 8), Pos(4, 7))
+        }
+
+        @Test
+        fun `should be OK for neighbours via teleport`() {
+            assertThat(game.accessibleNeighbours(Pos(0, 7))).contains(Pos(1, 7), Pos(30, 7))
+            assertThat(game.accessibleNeighbours(Pos(30, 7))).contains(Pos(29, 7), Pos(0, 7))
+        }
+    }
+
+    @Nested
+    inner class ComputeShortestPath {
+        @Test
+        fun `should be empty for incorrect input or output`() {
+            assertThat(game.computeShortestPath(Pos(0, 0), Pos(1, 2))).isEmpty()
+            assertThat(game.computeShortestPath(Pos(1, 2), Pos(0, 0))).isEmpty()
+        }
+
+        @Test
+        fun `should be empty for immediate neighbours`() {
+            assertThat(game.computeShortestPath(Pos(1, 1), Pos(1, 2))).isEmpty()
+            assertThat(game.computeShortestPath(Pos(1, 1), Pos(2, 1))).isEmpty()
+        }
+
+        @Test
+        fun `should be OK for random accessible point`() {
+            assertThat(game.computeShortestPath(Pos(1, 1), Pos(7, 1))).isEqualTo(horizontal(1, 2, 6))
+            assertThat(game.computeShortestPath(Pos(8, 5), Pos(8, 9))).isEqualTo(vertical(7, 5, 9))
+        }
+
+        @Test
+        fun `should be OK for random accessible point through teleport`() {
+            assertThat(game.computeShortestPath(Pos(5, 7), Pos(25, 7))).isEqualTo(horizontal(7, 4, 0) + horizontal(7, 30, 26))
+        }
+
+        @Test
+        fun `should be equal regarding to horizontal symetry`() {
+            val middleRow = 31 / 2
+            val path = game.computeShortestPath(Pos(1, 1), Pos(middleRow, 1))
+            val translatedPath = path.map { (x, y) -> Pos(2 * middleRow - x, y) }
+            val symetricPath = game.computeShortestPath(Pos(29, 1), Pos(middleRow, 1))
+            assertThat(translatedPath).isEqualTo(symetricPath)
+        }
     }
 
     @Nested
     inner class ComputeLinesOfSight {
         @Test
         fun `should be OK when there is 1 direction`() {
-            val pac = Pac(0, true, Point(29, 14), Point(0, 0), PacType.PAPER, 0, Point(0, 0))
+            val pac = Pac(0, true, Pos(29, 14), Pos(0, 0), PacType.PAPER, 0, Pos(0, 0))
             game.pacs = setOf(pac)
 
             game.computeLineOfSights()
@@ -45,12 +97,12 @@ internal class GameTest {
             assertThat(pac.linesOfSight.getValue(Direction.WEST)).isEmpty()
             assertThat(pac.linesOfSight.getValue(Direction.EAST)).isEmpty()
             assertThat(pac.linesOfSight.getValue(Direction.SOUTH)).isEmpty()
-            assertThat(pac.linesOfSight.getValue(Direction.NORTH)).containsExactly(Point(29, 13))
+            assertThat(pac.linesOfSight.getValue(Direction.NORTH)).containsExactly(Pos(29, 13))
         }
 
         @Test
         fun `should be OK when there are 2 directions`() {
-            val pac = Pac(0, true, Point(29, 13), Point(0, 0), PacType.PAPER, 0, Point(0, 0))
+            val pac = Pac(0, true, Pos(29, 13), Pos(0, 0), PacType.PAPER, 0, Pos(0, 0))
             game.pacs = setOf(pac)
 
             game.computeLineOfSights()
@@ -58,13 +110,13 @@ internal class GameTest {
             assertThat(pac.linesOfSight).hasSize(4)
             assertThat(pac.linesOfSight.getValue(Direction.WEST)).isEqualTo(horizontal(13, 28, 25))
             assertThat(pac.linesOfSight.getValue(Direction.EAST)).isEmpty()
-            assertThat(pac.linesOfSight.getValue(Direction.SOUTH)).containsExactly(Point(29, 14))
+            assertThat(pac.linesOfSight.getValue(Direction.SOUTH)).containsExactly(Pos(29, 14))
             assertThat(pac.linesOfSight.getValue(Direction.NORTH)).isEmpty()
         }
 
         @Test
         fun `should be OK when there are 3 directions`() {
-            val pac = Pac(0, true, Point(21, 11), Point(0, 0), PacType.PAPER, 0, Point(0, 0))
+            val pac = Pac(0, true, Pos(21, 11), Pos(0, 0), PacType.PAPER, 0, Pos(0, 0))
             game.pacs = setOf(pac)
 
             game.computeLineOfSights()
@@ -78,7 +130,7 @@ internal class GameTest {
 
         @Test
         fun `should be OK when there are 4 directions`() {
-            val pac = Pac(0, true, Point(25, 7), Point(0, 0), PacType.PAPER, 0, Point(0, 0))
+            val pac = Pac(0, true, Pos(25, 7), Pos(0, 0), PacType.PAPER, 0, Pos(0, 0))
             game.pacs = setOf(pac)
 
             game.computeLineOfSights()
@@ -91,19 +143,19 @@ internal class GameTest {
         }
     }
 
-    private fun horizontal(row: Int, colStart: Int, colEnd: Int): List<Point> {
+    private fun horizontal(row: Int, colStart: Int, colEnd: Int): List<Pos> {
         val range = if (colStart <= colEnd)
             IntRange(colStart, colEnd)
         else
             IntRange(colEnd, colStart).reversed()
-        return range.map { Point(it, row) }
+        return range.map { Pos(it, row) }
     }
 
-    private fun vertical(col: Int, rowStart: Int, rowEnd: Int): List<Point> {
+    private fun vertical(col: Int, rowStart: Int, rowEnd: Int): List<Pos> {
         val range = if (rowStart <= rowEnd)
             IntRange(rowStart, rowEnd)
         else
             IntRange(rowEnd, rowStart).reversed()
-        return range.map { Point(col, it) }
+        return range.map { Pos(col, it) }
     }
 }
